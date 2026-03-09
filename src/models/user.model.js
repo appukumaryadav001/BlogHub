b8c731d78c9812014df71e2a2b5a4e4c25d82c61
Appukumaryadav001 <appukumaryadav19@gmail.com>
2026-03-09 11:19:16 +0530

    fix bug

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -38,7 +38,7 @@ userSchema.methods.isPasswordCorrect = async function(password){
      return await bcrypt.compare(password,this.password);
 }
 
-userSchema.generateAccessToken = function(){
+userSchema.methods.generateAccessToken = function(){
   return jwt.sign(
     {
       _id:this._id,
