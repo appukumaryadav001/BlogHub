c4e0617a50d1f97073749fa76fc86275fe587551
Appukumaryadav001 <appukumaryadav19@gmail.com>
2026-03-15 08:02:55 +0530

    add comment.model.js

---
+++ src/models/comment.model.js
@@ -0,0 +1,19 @@
+import {Schema, model} from "mongoose";
+
+const commentSchema = new Schema({
+     content:{
+        type:String,
+        required:true
+     },
+     blogId:{
+        type:Schema.Types.ObjectId,
+        ref:"Blog",
+     },
+     creadedBy:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+     }
+},{timestamps:true}
+);
+
+export const Comment = model("Comment",commentSchema);
